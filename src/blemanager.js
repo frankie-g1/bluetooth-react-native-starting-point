db3d56f6fa6b838f8ac5efe67f37791c59c7ce0a
Guarino <guarinof@wit.edu>
2022-07-19 19:09:22 -0400

    first commit

---
+++ src/blemanager.js
@@ -0,0 +1,79 @@
+import {BleManager} from 'react-native-ble-plx';
+import React, {Component} from 'react'
+import {Text} from 'react-native'
+
+class BManager extends Component {
+
+    constructor(){
+        super();
+        this.manager = new BleManager();
+        
+    }
+
+    
+
+
+    componentWillMount() {
+        const subscription = this.manager.onStateChange((state) => {
+            if (state === 'PoweredOn') {
+                console.log("powered on")
+
+                this.scanAndConnect();
+                subscription.remove();
+            }
+            else{
+                console.log(state)
+            }
+        }, true);
+    }
+
+
+    scanAndConnect() {
+        this.manager.startDeviceScan(null, null, (error, device) => {
+            if (error) {
+                console.log("error")
+                console.log(error)
+                // Handle error (scanning will be stopped automatically)
+                return
+            }
+    
+            // Check if it is a device you are looking for based on advertisement data
+            // or other criteria.
+            if (device.name === 'JLab GO Air') {
+
+                    console.log(device.name)
+                
+                // Stop scanning as it's not necessary if you are scanning for one device.
+                this.manager.stopDeviceScan();
+    
+                // Proceed with connection.
+            }
+            device.connect()
+            .then((device) => {
+                console.log(device)
+                return device.discoverAllServicesAndCharacteristics()
+            })
+            .then((device) => {
+            // Do work on device with services and characteristics
+            })
+            .catch((error) => {
+                console.log("error")
+                console.log(error)
+                // Handle errors
+            });
+        });
+    }
+    
+
+    render(){
+
+        return(
+            <Text>BLE MANAGER </Text>
+        )
+    }
+
+
+
+}
+
+export default BManager;
---
+++ src/bleprocess.js
@@ -0,0 +1,62 @@
+import React, {useEffect} from 'react'
+
+
+const bleprocess = ({manager}) => {
+
+        useEffect(() => {
+            const subscription = manager.onStateChange((state) => {
+                if (state === 'PoweredOn') {
+                    this.scanAndConnect();
+                    subscription.remove();
+                }
+            }, true);
+            return () => subscription.remove();
+        }, [manager]);
+
+
+        const scanAndConnect = () => {
+            manager.startDeviceScan(null, null, (error, device) => {
+                if (error) {
+                    // Handle error (scanning will be stopped automatically)
+                    return
+                }
+        
+                // Check if it is a device you are looking for based on advertisement data
+                // or other criteria.
+                if (device.name === 'JLab Air Pro' || 
+                    device.name === 'SensorTag') {
+                    
+                    // Stop scanning as it's not necessary if you are scanning for one device.
+                    manager.stopDeviceScan();
+        
+                    // Proceed with connection.
+                }
+            });
+        }
+
+        device.connect()
+    .then((device) => {
+        console.log(device)
+        return device.discoverAllServicesAndCharacteristics()
+    })
+    .then((device) => {
+       // Do work on device with services and characteristics
+    })
+    .catch((error) => {
+        // Handle errors
+    });
+
+
+        return (
+            <div>bleprocess</div>
+        )
+        
+
+            
+}
+
+
+export default bleprocess;
+
+
+
